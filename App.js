b1d1b48f875f0c24f858a2a0d64a45c6e36449b0
keyobs <elodiekoob@gmail.com>
2024-01-25 15:43:04 +0100

    base: folder src + index

--- App.js
+++ App.js
@@ -1,20 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Root from "./src/index";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>who ohh!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Root />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
