895f70826b4eaa5a934ff6a855f0d6b6c5734c0a
keyobs <elodiekoob@gmail.com>
2024-01-26 23:53:13 +0100

    base: clean navigation

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,27 @@
+import { StyleSheet } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
 import './translations/configI18n';
 
+import { StatusBar } from 'expo-status-bar';
+
 import RootNavigator from '@navigation/Navigation';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
-    return <RootNavigator />;
+    return (
+        <GestureHandlerRootView style={styles.container}>
+            <SafeAreaProvider>
+                <StatusBar style="auto" />
+                <RootNavigator />
+            </SafeAreaProvider>
+        </GestureHandlerRootView>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+});
--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,28 +1,13 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import TabsNavigator from './TabsNavigator';
-
-const Stack = createNativeStackNavigator();
 
 const RootNavigator = () => {
     return (
         <NavigationContainer>
-            <Router />
+            <TabsNavigator />
         </NavigationContainer>
     );
 };
 
 export default RootNavigator;
-
-export function Router() {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen
-                options={{ headerShown: false }}
-                name="App"
-                component={TabsNavigator}
-            />
-        </Stack.Navigator>
-    );
-}
