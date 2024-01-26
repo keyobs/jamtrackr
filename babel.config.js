cc09bf6746c106298f368319af86160814bd97ca
keyobs <elodiekoob@gmail.com>
2024-01-26 10:29:46 +0100

    config: aliases

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,33 @@
 module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+    api.cache(true);
+
+    return {
+        presets: ['babel-preset-expo'],
+    
+        env: {
+            production: {
+                plugins: ['react-native-paper/babel'], // smaller bundle size
+            },
+        },
+        plugins: [
+            [
+                'module-resolver',
+                {
+                    alias: {
+                        assets: './src/assets',
+                        components: './src/components',
+                        constants: './src/constants',
+                        hooks: './src/hooks',
+                        libs: './src/libs',
+                        navigation: './src/navigation',
+                        screens: './src/screens',
+                        services: './src/services',
+                        store: './src/store',
+                        templates: './src/templates',
+                    },
+                }
+            ],
+        ],
+
+    };
 };
