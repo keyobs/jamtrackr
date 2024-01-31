9682fae3a3925a1b7416435fb7c48bac3f7440fc
keyobs <elodiekoob@gmail.com>
2024-01-31 11:34:16 +0100

    players: style basic structure

--- src/screens/league/players/PlayersScreen.tsx
+++ src/screens/league/players/PlayersScreen.tsx
@@ -1,22 +1,59 @@
-import { Text, StyleSheet, View } from 'react-native';
+import {
+    Text,
+    StyleSheet,
+    View,
+    StyleProp,
+    TextStyle,
+    ViewStyle,
+} from 'react-native';
 import { themeColors } from '@theme/colors';
 
 const PlayersScreen = () => {
     return (
         <View style={styles.container}>
-            <Text>Create a new player</Text>
+            <View style={[styles.section, styles.playersList]}>
+                <Text style={styles.listTitle}>Liste</Text>
+                <Text>number name team</Text>
+                <Text></Text>
+            </View>
+
+            <View style={styles.section}>
+                <Text>Create a new player</Text>
+                <Text>name</Text>
+                <Text>number</Text>
+                <Text>role</Text>
+                <Text>team</Text>
+            </View>
         </View>
     );
 };
 
 export default PlayersScreen;
 
-const styles = StyleSheet.create({
+const styles: Record<
+  string,
+  StyleProp<ViewStyle | TextStyle>
+> = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: themeColors.darkBlue,
+        paddingTop: 15,
     },
-    text: {
+    section: {
+        flex: 2,
+    },
+    playersList: {
+        borderTopWidth: 1,
+        borderBottomWidth: 1,
+        borderTopColor: themeColors.ivory,
+        borderBottomColor: themeColors.ivory,
+    },
+    listTitle: {
         color: themeColors.ivory,
+        fontSize: 20,
+        paddingHorizontal: 20,
+        paddingVertical: 7,
+        borderBottomColor: themeColors.ivory,
+        borderBottomWidth: 1,
     },
 });
