3fbf2dd8acb8a41248bd43d4148a12046f059498
keyobs <elodiekoob@gmail.com>
2024-02-04 13:45:48 +0100

    players: display noItems

--- src/screens/league/players/PlayersScreen.tsx
+++ src/screens/league/players/PlayersScreen.tsx
@@ -14,7 +14,10 @@ import { useTranslation } from 'react-i18next';
 import { ListItem } from '@rneui/base';
 
 import { usePlayersStore } from '@store/playersStore';
+import { WideButtonWithIcon } from '@components/buttons/WideButtonWithIcon';
+
 import { CreatePlayerForm } from './CreatePlayerForm';
+import NoListItem from '@components/lists/NoListItem';
 
 const PlayersScreen = () => {
     const formRef = useRef(null);
@@ -22,52 +25,63 @@ const PlayersScreen = () => {
 
     const { playersList } = usePlayersStore((state) => state);
 
-    const [openCreatePlayerForm, setOpenCreatePlayerForm] = useState(false);
+    const [openCreatePlayerForm, setOpenCreatePlayerForm] = useState(true);
 
     return (
         <KeyboardAwareScrollView
-            keyboardShouldPersistTaps="always"
             style={styles.container}
+            keyboardShouldPersistTaps="always"
             getTextInoutRefs={() => formRef}
         >
             <View style={styles.content}>
-                <View style={[styles.section, styles.playersList]}>
-                    {[...playersList].map((player) => (
-                        <ListItem key={player.name}>
-                            <ListItem.Content>
-                                <ListItem.Title>{player.name}</ListItem.Title>
-                            </ListItem.Content>
-                        </ListItem>
-                    ))}
-                </View>
+                <WideButtonWithIcon
+                    label={t('players_to_players_list_button_label')}
+                    preset="chevron-next"
+                    onPressAction={() => alert('to the players list')}
+                />
 
-                <ListItem.Accordion
-                    containerStyle={{
-                        backgroundColor: themeColors.pink,
-                        borderBottomColor: themeColors.ivory,
-                        borderBottomWidth: 1,
-                    }}
-                    icon={{
-                        type: 'feather',
-                        name: 'chevron-down',
-                        color: themeColors.ivory,
-                    }}
-                    content={
-                        <>
-                            <ListItem.Content>
-                                <ListItem.Title style={{ color: themeColors.ivory }}>
-                                    {t('players_form_title')}
-                                </ListItem.Title>
-                            </ListItem.Content>
-                        </>
-                    }
-                    isExpanded={openCreatePlayerForm}
-                    onPress={() => setOpenCreatePlayerForm(!openCreatePlayerForm)}
-                >
-                    <View ref={formRef} style={{ paddingTop: 20 }}>
-                        <CreatePlayerForm />
-                    </View>
-                </ListItem.Accordion>
+                {playersList.length > 0 ? (
+                    <View
+                        style={{
+                            minHeight: 200,
+                        }}
+                    ></View>
+                ) : (
+                    <NoListItem
+                        disclaimerText={t('players_no_players_disclaimer')}
+                        iconSize={60}
+                    />
+                )}
+
+                <View>
+                    <ListItem.Accordion
+                        containerStyle={{
+                            backgroundColor: themeColors.pink,
+                            borderBottomColor: themeColors.ivory,
+                            borderBottomWidth: 1,
+                        }}
+                        icon={{
+                            type: 'feather',
+                            name: 'chevron-down',
+                            color: themeColors.ivory,
+                        }}
+                        content={
+                            <>
+                                <ListItem.Content>
+                                    <ListItem.Title style={{ color: themeColors.ivory }}>
+                                        {t('players_form_title')}
+                                    </ListItem.Title>
+                                </ListItem.Content>
+                            </>
+                        }
+                        isExpanded={openCreatePlayerForm}
+                        onPress={() => setOpenCreatePlayerForm(!openCreatePlayerForm)}
+                    >
+                        <View ref={formRef} style={{ paddingTop: 20 }}>
+                            <CreatePlayerForm />
+                        </View>
+                    </ListItem.Accordion>
+                </View>
             </View>
         </KeyboardAwareScrollView>
     );
@@ -83,17 +97,20 @@ const styles: Record<
         flex: 1,
         backgroundColor: themeColors.darkBlue,
         paddingTop: 10,
-    },
-    content: {
-        flexGrow: 1,
-        justifyContent: 'space-between',
         paddingBottom: 50,
     },
+    content: {
+        flex: 1,
+        justifyContent: 'space-between',
+    },
     formTitle: {
-        color: themeColors.ivory,
         fontSize: 18,
         paddingHorizontal: 10,
         paddingVertical: 15,
+        backgroundColor: themeColors.ivory,
+        color: themeColors.darkBlue,
+        borderBottomWidth: 2,
+        borderBottomColor: themeColors.darkBlue,
     },
     section: {
         minHeight: 200,
